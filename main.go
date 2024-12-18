98ea69cc3bcc7d07f63f78c123b53074027baa27
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2024-12-18 20:57:02 +0700

    tool

--- main.go
+++ main.go
@@ -10,37 +10,47 @@ import (
 	"os"
 )
 
-var action string
+var action string = "upload"
+var apiUrl string = "http://myfinance.sonnguyen9800.com/api"
+var token string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5ndXllbmhzOTgwMEBnbWFpbC5jb20iLCJleHAiOjE3MzQ2MTI2NTQsInJvbGUiOiJ1c2VyIiwidXNlcl9pZCI6IjY3NjJjMmE3OTYwMGVlZGI5MGQyODE1YSJ9.EG8VNEAei3VsioFcQ9ucaCSJtvipt-nPNBQ8yANgSKg"
+var filePath string = "Payment2021.csv"
 
 func init() {
 	flag.StringVar(&action, "action", "", "action is required, either --upload or --download")
+	flag.StringVar(&apiUrl, "apiUrl", "", "apiUrl is required")
+	flag.StringVar(&token, "token", "", "token is required")
+	flag.StringVar(&filePath, "filePath", "", "filePath is required")
 	flag.Parse()
 	if action != "upload" && action != "download" {
 		fmt.Println("action is required, either --upload or --download")
 		os.Exit(1)
 	}
+	if apiUrl == "" {
+		fmt.Println("apiUrl is required")
+		os.Exit(1)
+	}
+	if token == "" {
+		fmt.Println("token is required")
+		os.Exit(1)
+	}
+	if filePath == "" {
+		fmt.Println("filePath is required")
+		os.Exit(1)
+	}
 }
 
 func main() {
-	var filePath string
-	if len(os.Args) > 2 {
-		filePath = os.Args[2]
-	} else {
-		fmt.Println("No filepath is provided")
-		os.Exit(1)
-	}
-
 	if action == "upload" {
-		DemoUploadCSV(filePath)
+		DemoUploadCSV(filePath, apiUrl, token)
 	} else if action == "download" {
-		DemoDownloadCSV(filePath)
+		DemoDownloadCSV(filePath, apiUrl, token)
 	}
 }
-func DemoDownloadCSV(filePath string) {
-	apiURL := "http://localhost:8080/api/expenses/download"
-	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFiY0BnbWFpbC5jb20iLCJleHAiOjE3MzM2NTg3OTcsInJvbGUiOiJ1c2VyIiwidXNlcl9pZCI6IjY3NGViZDZjODgzNDU4MTJkOThiNmEwYiJ9.UDpz35nx58XflMzie29glqy6J6m-gQWyULIDXz4OVZI"
 
-	req, err := http.NewRequest("GET", apiURL, nil)
+func DemoDownloadCSV(filePath string, apiUrl string, token string) {
+	apiUrl = apiUrl + "/expenses/download"
+
+	req, err := http.NewRequest("GET", apiUrl, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -78,9 +88,8 @@ func DemoDownloadCSV(filePath string) {
 	fmt.Println("CSV file saved to", filePath)
 }
 
-func DemoUploadCSV(filePath string) {
-	apiURL := "http://localhost:8080/api/expenses/upload"
-	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFiY0BnbWFpbC5jb20iLCJleHAiOjE3MzM2NTg3OTcsInJvbGUiOiJ1c2VyIiwidXNlcl9pZCI6IjY3NGViZDZjODgzNDU4MTJkOThiNmEwYiJ9.UDpz35nx58XflMzie29glqy6J6m-gQWyULIDXz4OVZI"
+func DemoUploadCSV(filePath string, apiUrl string, token string) {
+	apiUrl = apiUrl + "/expenses/upload"
 	file, err := os.Open(filePath)
 	if err != nil {
 		fmt.Println(err)
@@ -112,7 +121,7 @@ func DemoUploadCSV(filePath string) {
 	}
 
 	// Create the HTTP request
-	req, err := http.NewRequest("POST", apiURL, body)
+	req, err := http.NewRequest("POST", apiUrl, body)
 	if err != nil {
 		fmt.Printf("Error creating request: %v\n", err)
 		return
